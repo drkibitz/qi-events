a283d487bcbed915b3d8380239f5905bd6c2af15
Dr. Kibitz <info@drkibitz.com>
2014-02-12 22:20:59 -0800

    Update banner

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 /**
  * qi-events 0.0.5
  * http://drkibitz.github.io/qi-events/
- * Copyright (c) 2013 Dr. Kibitz, http://drkibitz.com
+ * Copyright (c) 2013-2014 Dr. Kibitz, http://drkibitz.com
  * A no nonsense event API (BASED ON Backbone.Events).
- * built: Wed Feb 12 2014 21:36:20 GMT-0800 (PST)
+ * built: Wed Feb 12 2014 22:19:57 GMT-0800 (PST)
  *
  * Backbone.js 0.9.10
  * (c) 2010-2013 Jeremy Ashkenas, DocumentCloud Inc.
